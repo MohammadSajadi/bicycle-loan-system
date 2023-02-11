64d99ec55d4b58f95417a9f791065f42a0814f19
Latif-Karimi <114200825+Latif-Karimi@users.noreply.github.com>
2023-02-11 11:12:12 -0500

    userlogin and registration

---
+++ Front-End/src/components/Login/Login.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+export const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const handleSubmite = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
+  return (
+    <div className="user-form">
+      <form className="login-form" onSubmit={handleSubmite}>
+        <label htmlFor="email">email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="youremail@abcd.com"
+          id="email"
+          name="email"
+        />
+        <label htmlFor="password">password</label>
+        <input
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="*********"
+          id="password"
+          name="password"
+        />
+        <button>Login</button>
+      </form>
+      <button className="btn-link" onClick={() => props.onFormSwitch("Register")}>
+        Don't have an account Register here!
+      </button>
+      </div>
+  );
+};
---
+++ Front-End/src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './AboutUs.css';
+
+export const AboutUs = () => (
+  <div className='app-aboutus app-bg flex-center section-padding' id="about">
+    
+    <div className='app-aboutus-content flex-center'>
+      <div className='app-aboutus-content-about'>
+        <h1 className='headtext-cormorant'>About Us</h1>
+        <p className='p-opensans'> we are dedicated to providing a sustainable and convenient mode of transportation for exploring the beautiful city of Montreal. Our mission is to make cycling accessible to everyone, while promoting a healthier and greener lifestyle. Our vision is to be the go-to provider of bike rentals in Montreal, known for our high-quality bikes, exceptional customer service, and commitment to the environment. Our values include sustainability, inclusivity, and a passion for promoting healthy, active lifestyles."</p>
+      </div>
+      <div className='app-aboutus-content-history'>
+        <h1 className='headtext-cormorant'>Our History</h1>
+        <p className='p-opensans'> "Founded in [Year?], [Company Name] started for renting bikes with a mission to encourage more people to explore the city on two wheels. Over the years, we have grown into a leading provider of bicycle rentals in Montreal, with a fleet of high-quality bikes and a team of knowledgeable and friendly staff. Our success is rooted in our commitment to sustainability, inclusivity, and a passion for promoting healthy, active lifestyles. From casual riders to serious cyclists, [Company Name] has something for everyone, and we look forward to continuing our mission of making cycling accessible to all for many years to come."</p>
+        
+      </div>
+    </div>
+
+  </div>
+);
+
+
