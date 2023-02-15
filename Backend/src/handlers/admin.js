f15df608861aa4ec041601b02879766e274fdeb4
MohammadSajadi <sajadi.mohamad89@gmail.com>
2023-02-14 19:59:43 -0500

    add email to user and admin register and sign in

--- Backend/src/handlers/admin.js
+++ Backend/src/handlers/admin.js
@@ -1,36 +1,31 @@
 const prisma = require('../db');
-const { createJWT, hashPassword, comparePasswords } = require("../modules/auth");
 
 const createNewAdmin = async (req, res) => {
-  const hash = await hashPassword(req.body.password);
 
-  const user = await prisma.user.create({
+  const admin = await prisma.admin.create({
     data: {
       username: req.body.username,
-      password: hash,
+      password: req.body.password,
+      email: req.body.email
     },
   });
 
-  const token = createJWT(user);
-  res.json({ token });
+  res.json(admin);
 };
 
 const adminSignin = async (req, res) => {
-    const user = await prisma.user.findUnique({
-      where: { username: req.body.username },
-    });
-  
-    const isValid = await comparePasswords(req.body.password, user.password);
-  
-    if (!isValid) {
-      res.status(401);
-      res.send("Invalid username or password");
-      return;
-    }
-  
-    const token = createJWT(user);
-    res.json({ token });
-  };
+  const admin = await prisma.admin.findUnique({
+    where: { username: req.body.username },
+  });
+
+  if (req.body.password === admin.password) {
+    res.json(admin);
+  } else {
+    res.status(401);
+    res.send("Invalid username or password");
+    return;
+  }
+};
 
   module.exports = {createNewAdmin,adminSignin}
   
--- Backend/src/handlers/user.js
+++ Backend/src/handlers/user.js
@@ -1,36 +1,31 @@
 const prisma = require('../db');
-const { createJWT, hashPassword, comparePasswords } = require("../modules/auth");
 
 const createNewUser = async (req, res) => {
-  const hash = await hashPassword(req.body.password);
 
   const user = await prisma.user.create({
     data: {
       username: req.body.username,
-      password: hash,
+      password: req.body.password,
+      email: req.body.email
     },
   });
 
-  const token = createJWT(user);
-  res.json({ token });
+  res.json(user);
 };
 
 const signin = async (req, res) => {
-    const user = await prisma.user.findUnique({
-      where: { username: req.body.username },
-    });
-  
-    const isValid = await comparePasswords(req.body.password, user.password);
-  
-    if (!isValid) {
-      res.status(401);
-      res.send("Invalid username or password");
-      return;
-    }
-  
-    const token = createJWT(user);
-    res.json({ token });
-  };
+  const user = await prisma.user.findUnique({
+    where: { username: req.body.username },
+  });
+
+  if (req.body.password === user.password) {
+    res.json(user);
+  } else {
+    res.status(401);
+    res.send("Invalid username or password");
+    return;
+  }
+};
 
   module.exports = {createNewUser,signin}
   
